1714914eb7df4cb038b2c34431373111e5fe317b
andapar <160366488+andapar@users.noreply.github.com>
2024-10-31 13:30:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Introducing Liman, our new AI-powered research assistants that manages Bond ETFs. &nbsp;
+          Introducing Liman, our new AI-powered research assistants that manages ETFs and Bist Stock. &nbsp;
           <code className="font-mono font-bold">limanai.com</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
@@ -48,13 +48,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Researh {" "}
+            Researh And Dev Docs {" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Macro/Micro and Mathematical 
+            Our
           </p>
         </a>
 
@@ -71,7 +71,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Our developed fund protocol under the MIT license.
+            Our developed decentralized money protocol under the MIT license.
           </p>
         </a>
 
@@ -88,7 +88,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            You can reach us on info@liman.bond
+            You can reach us on info@liman.finance
           </p>
         </a>
 
@@ -105,7 +105,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Bond ETF focus Ai Research Assistant -
+            Bist and ETF focus Ai Research Assistant -
           </p>
         </a>
       </div>
